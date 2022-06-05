7f0d2b20f530ed1e65fe78f23f7d5b86105a30ed
Reingor <96390685+Reingor@users.noreply.github.com>
2022-06-05 16:28:55 +0300

    Create models

---
+++ CoinApp/Models/AssetsModel.cs
@@ -0,0 +1,10 @@
+﻿namespace CoinsApp.Model
+{
+
+    public class AssetsModel
+    {
+        public InfoAssetModel[] Data { get; set; }
+        public long Timestamp { get; set; }
+    }
+
+}
---
+++ CoinApp/Models/MarketsModel.cs
@@ -0,0 +1,12 @@
+﻿namespace CoinsApp.Model
+{
+
+
+
+    public class MarketsModel
+    {
+        public InfoMarket[] Data { get; set; }
+        public long Timestamp { get; set; }
+    }
+
+}
